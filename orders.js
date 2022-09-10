5fc2a6edcb7065b1f5fb8fab3b54fcdcc4124329
Kyle Wilson <tau421@gmail.com>
2022-09-10 10:54:34 -0500

    orders done

--- orders.js
+++ orders.js
@@ -30,7 +30,18 @@
 */
 
 //CODE HERE
-
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
+    updateStatus(newStatus){
+        this.status === newStatus;
+        console.log('The order for', this.customerId, 'is now', newStatus,'.')
+    }
+}
 
 
 /*
@@ -45,7 +56,7 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket('pizza, bread, and soda', '7:03 PM', 575);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +65,4 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking')
